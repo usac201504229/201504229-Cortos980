083bcbceb73aa923741c6b0d3fe1dc2bff93e1f4
usac201504229 <2468064780101@ingenieria.usac.edu.gt>
2020-06-09 12:47:49 -0600

    Segunda version de corto2

--- corto1/corto1.py
+++ corto1/corto1.py
@@ -1,12 +1,18 @@
 n = int(input("Ingresar numero: ")) #Ingresar el valor a evaluar
 
+lista =[]
+
 while n != 1:          #Que esl ciclo funcione si los valores de n es diferente a 1
     if n % 2 == 0:     #Si el numero es par
-        print("%d," % (n),end= = "") #formato de cadenas con valor de n, imprima los valores en linea
-        n = n / 2
-    else:
-        print("%d," % (n),end = "")
-        n = (n * 3) + 1
+        print("%d," %(n),end = "") #formato de cadenas con valor de n, imprima los valores en linea
+        n = n / 2       #Como es par el valor lo divide en 2
+    else:               #De lo contario 
+        print("%d," %(n),end = "")  
+        n = (n * 3) + 1     # Valor diferente multiplica por 3 y suma 1
 
-    if n == 1:
+    if n <= 1:
         print("1")
+
+    for n in lista:
+        f.write(n)
+    f.close()
